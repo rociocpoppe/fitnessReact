cf1133e68009645a83ee062da4eb2a77ceccddd0
Rocio Poppe <rociocpoppe@gmail.com>
2020-12-29 09:56:41 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+//SIN REACT
+
+/*const element=document.createElement('h1')
+element.innerText='Hello React..'
+
+const container=document.getElementById('root')
+container.appendChild(element)
+*/
+
+//CON REACT
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const user={
+  firstname:'Rocio',
+  lastname:'Poppe',
+  //avatar:'https://avatar.tobi.sh/'
+};
+
+function getName(user){
+  return user.firstname +' ' + user.lastname 
+}
+
+function getGreeting(user){
+  if(user){
+    return <h1>Hello {getName(user)}!</h1>
+  }
+  return <h1>Hello Stranger!</h1>
+}
+const element=  
+  <div>
+    <h1>{getGreeting(user)}</h1>
+  </div>
+
+const container=document.getElementById('root')
+
+ReactDOM.render(element,container)
